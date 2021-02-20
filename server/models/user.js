ae106d741e383ef5b4534f47de90f410fd2b6c7b
KT123 <krishnateja@monocept.com>
2021-02-21 01:15:32 +0530

    role in model

--- server/models/user.js
+++ server/models/user.js
@@ -26,6 +26,10 @@ const UserSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  role: {
+    type: String,
+    required: true,
+  },
   phone: {
     type: String,
     required: true,
